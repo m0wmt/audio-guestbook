b2df2d72e81c547fccdebadb61bb8301d9a575b6
m0wmt <nobody@example.com>
2025-02-01 15:03:32 +0000

    Remove port mapping

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,7 @@
  * Requirements are available in the README.md file.
  */
 #include <Arduino.h>
-#include <Wire.h>
 
-/* Headers to map pins to ports */
-#include <algorithm>
-#include "pin.h"
 
 /* Variables to obtain free RAM */
 extern unsigned long _heap_start;
@@ -19,7 +15,6 @@ extern char *__brkval;
 
 /* Function prototypes */
 static int free_ram(void);
-static void map_pins_to_ports(void);
 
 /**
  * @brief Program setup
@@ -31,8 +26,6 @@ void setup() {
 
   Serial.print("Free RAM = ");
   Serial.println(free_ram());
-
-  map_pins_to_ports();
 }
 
 /**
@@ -49,46 +42,4 @@ void loop() {
  */
 static int free_ram(void) {
     return (char *)&_heap_end - __brkval;
-}
-
-/**
- * @brief Display GPIO registers to the digital pins and vice versa.
- */
-static void map_pins_to_ports(void) {
-    Pin *pins1[CORE_NUM_DIGITAL];
-    Pin *pins2[CORE_NUM_DIGITAL];
-
-    for (unsigned pinNr = 0; pinNr < CORE_NUM_DIGITAL; pinNr++)
-    {
-        Pin *p = new Pin(pinNr);
-        pins1[pinNr] = p;
-        pins2[pinNr] = p;
-    }
-
-    std::sort(pins1, pins1 + CORE_NUM_DIGITAL, [](Pin *a, Pin *b)  // Sort by pin number
-    {
-        return a->getPinNr() < b->getPinNr(); 
-    }); 
-
-    std::sort(pins2, pins2 + CORE_NUM_DIGITAL, [](Pin *a, Pin *b)   // Sort by GPIO and Bit
-    {
-        if (a->getGpioNr() < b->getGpioNr()) return true;
-        if (a->getGpioNr() > b->getGpioNr()) return false;
-        if (a->getBitNr() < b->getBitNr())   return true;
-        return false;
-    });
-
-    // Print results in two columns--------------------------
-    Serial.println("PIN   GPIOn-BITm  |  GPIOn-BITm    PIN");
-    Serial.println("------------------|-------------------");
-    for (unsigned i = 0; i < CORE_NUM_DIGITAL; i++)
-    {
-        unsigned pin1 = pins1[i]->getPinNr();
-        unsigned pin2 = pins2[i]->getPinNr();
-        unsigned gpio1 = pins1[i]->getGpioNr();
-        unsigned gpio2 = pins2[i]->getGpioNr();
-        unsigned bit1 = pins1[i]->getBitNr();
-        unsigned bit2 = pins2[i]->getBitNr();
-        Serial.printf("%02d  -> GPIO%u-%02u   |   GIPO%u-%02u  ->  %02d\n", pin1, gpio1, bit1, gpio2, bit2, pin2);
-    }    
 }
