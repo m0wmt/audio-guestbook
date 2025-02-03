0cbb2fe8e6b30b4425015b5242bf89b18cf55b94
m0wmt <nobody@example.com>
2025-02-03 14:25:24 +0000

    Handset and PRESS button pins set

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,20 @@
 #include <SD.h>
 #include <SerialFlash.h>
 #include <Audio.h>
+#include <Bounce.h>
 
 #include "play_sd_wav.h"
 
-// GLOBALS
+// Defines
+// Use Teensy 4.1 SD card, teensy forum thinks it is faster than the audio board SD card interface.
+#define SDCARD_CS_PIN    BUILTIN_SDCARD
+#define SDCARD_MOSI_PIN  11
+#define SDCARD_SCK_PIN   13
 
+#define HOOK_PIN 33         // Handset switch
+#define PRESS_PIN 34        // PRESS switch
+
+// Globals
 // All audio should be WAV files (44.1kHz 16-bit PCM)
 AudioPlaySdWav           playWav1;      
 AudioOutputI2S           audioOutput;   // I2S interface to Speaker/Line Out on Teensy 4.0 Audio shield
@@ -24,12 +33,10 @@ AudioConnection          patchCord2(playWav1, 1, audioOutput, 1);
 AudioControlSGTL5000     sgtl5000_1;
 AudioSynthWaveform       waveform1;     // To create the "beep" sfx
 
-// Use Teensy 4.1 SD card, teensy forum thinks it is faster than the audio board SD card interface.
-#define SDCARD_CS_PIN    BUILTIN_SDCARD
-#define SDCARD_MOSI_PIN  11
-#define SDCARD_SCK_PIN   13
 
-
+// Use long 40ms debounce time on switches
+Bounce buttonRecord = Bounce(HOOK_PIN, 40);
+Bounce buttonPress = Bounce(PRESS_PIN, 40);
 
 /* Function prototypes */
 static void play_file(const char *filename);
@@ -43,6 +50,10 @@ void setup() {
     } 
 
     Serial.println("Setting up audio and SD card");
+
+    // Configure the input pins
+    pinMode(HOOK_PIN, INPUT_PULLUP);
+    pinMode(PRESS_PIN, INPUT_PULLUP);
 
     // Reset the maximum reported by AudioMemoryUsageMax
     AudioMemoryUsageMaxReset();
@@ -66,16 +77,16 @@ void setup() {
     }
 
     // filenames are always uppercase 8.3 format
-    Serial.println("Starting to play example .wav files");
+    //Serial.println("Starting to play example .wav files");
     //play_file("SDTEST1.WAV");  
     //delay(500);
     //play_file("SDTEST2.WAV");
     //delay(500);
-    play_file("SDTEST3.WAV");
-    delay(500);
+    //play_file("SDTEST3.WAV");
+    //delay(500);
     // play_file("SDTEST4.WAV");
     // delay(1500);  
-    Serial.println("Example .wav files finished playing");
+    //Serial.println("Example .wav files finished playing");
 
     // Get the maximum number of blocks that have ever been used
     Serial.print("Max number of blocks used by Audio were: ");
@@ -86,6 +97,28 @@ void setup() {
  * @brief Main loop
  */
 void loop() {
+    // Read the buttons - can we move these to an interrupt?
+    buttonRecord.update();
+    buttonPress.update();
+
+    // Falling edge occurs when the handset is lifted --> GPO 706 telephone
+    if (buttonRecord.fallingEdge()) {
+        Serial.println("Handset lifted");
+        delay(1000);
+    } else if (buttonRecord.risingEdge()) {  // Handset is replaced
+        Serial.println("Handset replaced");
+        delay(1000);
+    }
+
+    // Falling edge occurs when the PRESS button is lifted --> GPO 706 telephone
+    if (buttonPress.fallingEdge()) {
+        Serial.println("PRESS button pressed");
+        delay(1000);
+    } else if (buttonPress.risingEdge()) {  // PRESS button is released
+        Serial.println("PRESS button released");
+        delay(1000);
+    }
+
 }
 
 
