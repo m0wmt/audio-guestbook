7d86359261f9e0f4d5883452f79de90b2b8b58fc
m0wmt <nobody@example.com>
2025-03-23 15:34:55 +0000

    Initial monitor idea

---
+++ admin-monitor/include/index.h
@@ -0,0 +1,69 @@
+const char MAIN_page[] PROGMEM = R"=====(
+<!DOCTYPE HTML><html>
+<head>
+  <title>Audio Guestbook</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+    html {font-family: Arial; display: inline-block; text-align: center;}
+    p { font-size: 1.2rem;}
+    body {  margin: 0;}
+    .topnav { overflow: hidden; background-color: #50B8B4; color: white; font-size: 1rem; }
+    .content { padding: 20px; }
+    .card { background-color: white; box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5); }
+    .cards { max-width: 800px; margin: 0 auto; display: grid; grid-gap: 2rem; grid-template-columns: repeat(auto-fit, minmax(200px, 1fr)); }
+    .reading { font-size: 1.4rem; }
+  </style>
+</head>
+<body>
+  <div class="topnav">
+    <h1>Audio Guestbook</h1>
+  </div>
+  <div class="content">
+    <div class="cards">
+      <div class="card">
+        <p style="color:rgb(10, 66, 64);">FIRMWARE</p><p><span class="reading"><span id="status">%STATUS%</span></span></p>
+      </div>
+      <div class="card">
+        <p style="color:rgb(10, 66, 64);">RECORDINGS</p><p><span class="reading"><span id="rec">%RECORDINGS%</span></span></p>
+      </div>
+      <div class="card">
+        <p style="color:rgb(10, 66, 64);">DISK SPACE</p><p><span class="reading"><span id="disk">%DISKSPACE%</span> Gb</span></p>
+      </div>
+    </div>
+  </div>
+<script>
+if (!!window.EventSource) {
+ var source = new EventSource('/events');
+ 
+ source.addEventListener('open', function(e) {
+  console.log("Events Connected");
+ }, false);
+ source.addEventListener('error', function(e) {
+  if (e.target.readyState != EventSource.OPEN) {
+    console.log("Events Disconnected");
+  }
+ }, false);
+ 
+ source.addEventListener('message', function(e) {
+  console.log("message", e.data);
+ }, false);
+ 
+ source.addEventListener('status', function(e) {
+  console.log("status", e.data);
+  document.getElementById("stat").innerHTML = e.data;
+ }, false);
+ 
+ source.addEventListener('recordings', function(e) {
+  console.log("recordings", e.data);
+  document.getElementById("rec").innerHTML = e.data;
+ }, false);
+ 
+ source.addEventListener('diskspace', function(e) {
+  console.log("diskspace", e.data);
+  document.getElementById("disk").innerHTML = e.data;
+ }, false);
+}
+</script>
+</body>
+</html>
+)=====";
--- admin-monitor/src/main.cpp
+++ admin-monitor/src/main.cpp
@@ -1,18 +1,130 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Load Wi-Fi library
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+//#include <WebServer.h>
+#include <WiFi.h>
+
+#include "../include/index.h"
+
+static String processor(const String &var);
+    
+// Replace with your network credentials
+const char *ssid = "Guestbook-AP";
+const char *password = "123456789"; // temp password!
+
+// Set web server port number to 80
+AsyncWebServer server(80);
+
+// Create an Event Source on /events
+AsyncEventSource events("/events");
+
+// Timer variables
+unsigned long last_time = 0;
+unsigned long timer_delay = 30000;
+float disk_space = 14.0;
+unsigned int recordings = 7;
+unsigned int status = 1;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+    // TODO: Turn off all LEDs to save power
+
+    Serial.begin(115200);
+
+    delay(3000);
+
+    Serial.println(F("\n##################################"));
+    Serial.println(F("ESP32 Information:"));
+    Serial.printf("Internal Total Heap %d, Internal Used Heap %d, Internal Free Heap %d\n", ESP.getHeapSize(),
+                  ESP.getHeapSize() - ESP.getFreeHeap(), ESP.getFreeHeap());
+    Serial.printf("Sketch Size %d, Free Sketch Space %d\n", ESP.getSketchSize(), ESP.getFreeSketchSpace());
+    Serial.printf("SPIRam Total heap %d, SPIRam Free Heap %d\n", ESP.getPsramSize(), ESP.getFreePsram());
+    Serial.printf("Chip Model %s, ChipRevision %d, Cpu Freq %d, SDK Version %s\n", ESP.getChipModel(),
+                  ESP.getChipRevision(), ESP.getCpuFreqMHz(), ESP.getSdkVersion());
+    Serial.printf("Flash Size %d, Flash Speed %d\n", ESP.getFlashChipSize(), ESP.getFlashChipSpeed());
+    Serial.println(F("##################################\n\n"));
+
+    // Connect to Wi-Fi network with SSID and password
+    Serial.println("Setting AP (Access Point)…");
+
+    // Remove the password parameter, if you want the AP (Access Point) to be open
+    WiFi.softAP(ssid, password);
+
+    Serial.println("Connecting to ");
+    Serial.print(ssid);
+
+    // If connection successful show IP address in serial monitor
+    Serial.println("");
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+
+    IPAddress IP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(IP);
+
+    // Handle Web Server
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send(200, "text/html", MAIN_page, processor); });
+
+    // Handle Web Server Events
+    events.onConnect([](AsyncEventSourceClient *client) {
+        if (client->lastId()) {
+            Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
+        }
+        // send event with message "hello!", id current millis
+        // and set reconnect delay to 1 second
+        client->send("hello!", NULL, millis(), 10000);
+    });
+    server.addHandler(&events);
+    server.begin(); // Start server
+
+    Serial.println("HTTP server started");
+    Serial.println("");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    if ((millis() - last_time) > timer_delay) {
+        disk_space -= 0.1;
+        recordings += 1;
+        Serial.printf("Disk Space = %.2f Gb", disk_space);
+        Serial.println();
+        Serial.printf("Recordings= %d", recordings);
+        Serial.println();
+        Serial.printf("Status = %d", status);
+        Serial.println();
+
+        // Send Events to the Web Client with the Sensor Readings
+        events.send("ping", NULL, millis());
+        events.send(String(disk_space).c_str(), "diskspace", millis());
+
+        if (status == 1)
+            events.send("OK", "status", millis());
+        else
+            events.send("ERROR", "status", millis());
+
+        events.send(String(recordings).c_str(), "recordings", millis());
+
+        last_time = millis();
+    }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+/**
+ * @brief Handle requests from the web page
+ */
+static String processor(const String &var) {
+    Serial.println(var);
+    
+    if (var == "DISKSPACE") {
+        return String(disk_space);
+    } else if (var == "STATUS") {
+        if (status == 1)
+            return "OK";
+        else
+            return "ERROR";
+    } else if (var == "RECORDINGS") {
+        return String(recordings);
+    }
+    return String();
 }
