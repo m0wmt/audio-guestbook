5b41a0477e28f4789b7df5557f94ba2d22134911
m0wmt <nobody@example.com>
2026-07-28 12:16:35 +0100

    Fix access point ip address, add phone hotspot capability.

--- admin-monitor/src/main.cpp
+++ admin-monitor/src/main.cpp
@@ -12,11 +12,21 @@
 #include <ESPAsyncWebServer.h>
 #include <WiFi.h>
 
+// Fix access point not working
+#include "esp_wifi.h"
+#include "esp_bt.h"
+
 #include "../include/index.h"
 #include "../include/config.h"
 
 #include <HardwareSerial.h>
 #include <inttypes.h>
+
+// the setup function runs once when you press reset or power the board
+#ifdef RGB_BUILTIN
+#undef RGB_BUILTIN
+#endif
+#define RGB_BUILTIN 48
 
 #define DEBUG true      // to turn on/off printf statements
 
@@ -24,11 +34,12 @@ static String processor(const String &var);
 static void send_events_to_web_client(void);
 
 // Teensy UART communications setup
-// Define the RX and TX pins for Serial 2
-#define RX_TEENSY 16
-#define TX_TEENSY 17 // Will not be used but set up
+// Define the RX pin for Serial
+// Using GPIO7 for uart, default tx/rx pins interact with access point wifi for some reason!
+#define RX_TEENSY 7
 #define TEENSY_BAUD_RATE 9600
-    HardwareSerial teensy_serial(0);
+HardwareSerial teensy_serial(0);
+
 typedef struct __attribute__((packed, aligned(1))) {
     uint8_t mode;
     uint16_t recordings;
@@ -53,6 +64,16 @@ AsyncWebServer server(80);
 // Create an Event Source on /events
 AsyncEventSource events("/events");
 
+// Set your Static IP address for hotspot
+IPAddress hotspot_local_ip(172, 20, 10, 10);
+IPAddress hotspot_gateway(172, 20, 10, 0);
+IPAddress hotspot_subnet(255, 255, 255, 240);
+
+// Define custom IP, Gateway, and Subnet for local access point
+IPAddress ap_local_ip(192, 168, 10, 1);
+IPAddress ap_gateway(192, 168, 10, 1);
+IPAddress ap_subnet(255, 255, 255, 0);
+
 // Variables
 unsigned long last_time = 0;
 char runtime_buffer[10];
@@ -60,7 +81,7 @@ char runtime_buffer[10];
 
 void setup() {
 
-    // TODO: Turn off all LEDs to save power
+    neopixelWrite(48, 0, 0, 0); // Off
 
     Serial.begin(115200);
 
@@ -78,26 +99,55 @@ void setup() {
         Serial.printf("Flash Size %d, Flash Speed %d\n", ESP.getFlashChipSize(), ESP.getFlashChipSpeed());
         Serial.println(F("##################################\n\n"));
     }
-    // Change CPU frequency to save power!
-
-    // Serial.println(F("\n##################################"));
-    // Serial.printf("Changing CPU frequency to 80MHz to save power\n");
-    // setCpuFrequencyMhz(80);
-    // Serial.printf("Cpu Freq Now %dMHz\n", ESP.getCpuFreqMHz());
-    // Serial.println(F("##################################\n\n"));
-    // delay(1000);    // Just to give time for everything to settle down
-
+ 
     // Connect to Wi-Fi network with SSID and password
-    Serial.println("Setting AP (Access Point)…");
+    if (DEBUG) {
+        Serial.println("Setting AP (Access Point)…");
+    }
+
+    // Connect to phone's hotspot connect, access point not working!
+    // WiFi.config(hotspot_local_ip, hotspot_gateway, hotspot_subnet);
+    // WiFi.mode(WIFI_STA); //Optional
+    // WiFi.begin(HOTSPOT_SSID, HOTSPOT_PASSWORD);
+    
+    // if (DEBUG) {
+    //     Serial.println("\nConnecting");
+    // }
+
+    // while(WiFi.status() != WL_CONNECTED){
+    //     if (DEBUG) {
+    //         Serial.print(".");
+    //     }
+
+    //     delay(100);
+    // }
+
+    // if (DEBUG) {
+    //     Serial.println("\nConnected to the WiFi network");
+    //     Serial.print("Local ESP32 IP: ");
+    //     Serial.println(WiFi.localIP());
+    // }
+
+    // // Set to off state to start as access point
+    WiFi.disconnect(true);
+    WiFi.mode(WIFI_OFF);
+
+    // Disable bluetooth as we're not using it
+    esp_bt_controller_disable();
+    // Lower wifi power to help with over heating
+    esp_wifi_set_max_tx_power(WIFI_POWER_11dBm);
+
+    // Set to access point mode
+    WiFi.mode(WIFI_AP);
+    // Set up custom IP address
+    WiFi.softAPConfig(ap_local_ip, ap_gateway, ap_subnet);
 
     // Taken from config.h
-    WiFi.softAP(SSID, WIFI_PASSWORD);
+    bool result = WiFi.softAP(SSID, WIFI_PASSWORD);
+    Serial.print("Set up access point result = ");
+    Serial.println(result);
 
     Serial.print("Connecting to: ");
-    Serial.println(SSID);
-
-    // If connection successful show IP address in serial monitor
-    Serial.print("Connected to: ");
     Serial.println(SSID);
 
     IPAddress IP = WiFi.softAPIP();
@@ -111,7 +161,9 @@ void setup() {
     // Handle Web Server Events
     events.onConnect([](AsyncEventSourceClient *client) {
         if (client->lastId()) {
-            Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
+            if (DEBUG) {
+                Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
+            }
         }
         // send event with message "hello!", id current millis
         // and set reconnect delay to 1 second
@@ -120,15 +172,17 @@ void setup() {
     server.addHandler(&events);
     server.begin(); // Start server
 
-    Serial.println("HTTP server started");
-    Serial.println("");
+    if (DEBUG) {
+        Serial.println("HTTP server started");
+        Serial.println("");
+    }
 
     // Set up run time buffer to 5 seconds, waiting time above!
     sprintf(runtime_buffer, "%02d:%02d:%02d", 0, 0, 5);
 
     // Set up UART communications (UART0) to Teensy. Rx only will be used,
     // there will be no transmit to the Teensy
-    teensy_serial.begin(TEENSY_BAUD_RATE, SERIAL_8N1);
+    teensy_serial.begin(TEENSY_BAUD_RATE, SERIAL_8N1, RX_TEENSY);
 
     audio_guestbook_data.disk_remaining = 0;
     audio_guestbook_data.recordings = 0;
@@ -145,45 +199,47 @@ void loop() {
                     // Serial.println(n);//debugg
                     uint32_t syncPatt = (uint32_t)teensy_serial.read() << 24 | (uint32_t)teensy_serial.read() << 16 |
                                         (uint32_t)teensy_serial.read() << 8 | teensy_serial.read();
-                    Serial.print("Sync: "); Serial.println(syncPatt, HEX);//debug
+                    if (DEBUG) {
+                        Serial.print("Sync: "); Serial.println(syncPatt, HEX);//debug
+                    }
                     delay(10); // without delay, the Receiver does not work! Why?
                     if (syncPatt == 0xDEADBEEF) {
                         byte p = teensy_serial.read(); // number of bytes in struct
                         byte m = teensy_serial.readBytes((byte *)&audio_guestbook_data, p);
                         
                         // Debug only printing
-                        Serial.print("Mode = ");
-                        switch (audio_guestbook_data.mode) {
-                            case ERROR:
-                                Serial.print("ERROR");
-                                break;
-
-                            case INITIALISING:
-                                Serial.print("INITIALISING");
-                                break;
-
-                            case READY:
-                                Serial.print("READY");
-                                break;
-
-                            case RECORDMESSAGEPROMPT:
-                                Serial.print("RECORDMESSAGEPROMPT");
-                                break;
-
-                            case RECORDING:
-                                Serial.print("RECORDING");
-                                break;
-
-                            case PLAYING:
-                                Serial.print("PLAYING");
-                                break;
-
-                            default:
-                                Serial.print("UNDEFINED");
-                                break;
-                        }
-
                         if (DEBUG) {
+                            Serial.print("Mode = ");
+                            switch (audio_guestbook_data.mode) {
+                                case ERROR:
+                                    Serial.print("ERROR");
+                                    break;
+
+                                case INITIALISING:
+                                    Serial.print("INITIALISING");
+                                    break;
+
+                                case READY:
+                                    Serial.print("READY");
+                                    break;
+
+                                case RECORDMESSAGEPROMPT:
+                                    Serial.print("RECORDMESSAGEPROMPT");
+                                    break;
+
+                                case RECORDING:
+                                    Serial.print("RECORDING");
+                                    break;
+
+                                case PLAYING:
+                                    Serial.print("PLAYING");
+                                    break;
+
+                                default:
+                                    Serial.print("UNDEFINED");
+                                    break;
+                            }
+
                             Serial.print("   ");
                             Serial.print("Recordings = ");
                             Serial.print(audio_guestbook_data.recordings);
